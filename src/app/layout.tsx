b6195f46088ebb7a1d21ddd8e0f46571de9d3514
artemnazarov <tema0690@gmail.com>
2026-01-12 11:48:01 +0700

    fix: remove background class from RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-bg-main text-text-primary`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-text-primary`}
       >
         {children}
       </body>
