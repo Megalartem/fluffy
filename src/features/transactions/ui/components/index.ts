a8e91b4dae687b5b9916d85f5113f6425b2c4606
artemnazarov <tema0690@gmail.com>
2026-01-15 18:29:53 +0700

    feat: create index file for transaction UI components export

---
+++ src/features/transactions/ui/components/index.ts
@@ -0,0 +1,7 @@
+
+
+export { CategoryField } from "./CategoryField/CategoryField";
+
+export { TransactionsFilter, type TransactionsFiltersValue } from "@/features/transactions/ui/components/TransactionsFilter/TransactionsFilter";
+
+export { TransactionUpsertSheet } from "./TransactionUpsertSheet/TransactionUpsertSheet";
