8ae25fa184941253727f463f2bb5d2b6358c44ba
artemnazarov <tema0690@gmail.com>
2026-02-03 12:29:57 +0700

    feat(index): export CategoryUpsertSheet component from index

--- src/features/categories/ui/molecules/index.ts
+++ src/features/categories/ui/molecules/index.ts
@@ -4,3 +4,4 @@ export { CategoryRow } from "./CategoryRow/CategoryRow";
 
 export { CategoryActionsMenu } from "./CategoryActionsMenu/CategoryActionsMenu";
 
+export { CategoryUpsertSheet } from "../components/CategoryUpsertSheet/CategoryUpsertSheet";
